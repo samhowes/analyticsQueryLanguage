a58fe343c9512f5ecdc912dbd4b08afe94c3208a
showes <showes06@gmail.com>
2020-11-26 20:47:34 -0500

    reverted for someone else's code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,6 @@ export class AppComponent implements AfterViewInit {
   title = 'analyticsQueryLanguage';
 
   ngAfterViewInit(){
-    var editor = monaco.editor.create(this.editorOutlet.nativeElement, {
-      value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-      language: 'c',
-      theme:'vs-dark'
-    });
+    
   }
 }
